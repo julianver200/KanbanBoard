2f5eb5740206a3994589279e1fd7b976e9702a87
julian <julianverquinal2006@gmail.com>
2026-06-11 15:40:34 +0800

    Added shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/button"
+
 export default function App() {
   return (
     <div className="underline">
-       Hello
+      <Button>Shadcn is working!</Button>
     </div>
   )
 }
